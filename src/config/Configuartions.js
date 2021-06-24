cd704bad2cc92eedc8b150e5f51ced15934200b4
Uzair <uzair@dynamiconlinetechnologies.com>
2021-06-24 21:18:46 +0500

    ...

--- src/config/Configuartions.js
+++ src/config/Configuartions.js
@@ -1,9 +1,9 @@
 class Configuration {
   // apiBaseUrl = 'https://autodeals-be.herokuapp.com/';
-  apiBaseUrl = "http://localhost:8000/"; //be2 is for second option if be free hours expired
+  apiBaseUrl = "https://habib-bill-be.herokuapp.com/"; //be2 is for second option if be free hours expired
   // apiBaseUrl = 'http://localhost:4000/';
   // hostUrl = 'http://localhost:3000';
-  hostUrl = "https://autodeals-web.firebaseapp.com";
+  hostUrl = "https://habib-bill-be.herokuapp.com/";
 }
 
 export default Configuration;
